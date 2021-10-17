cadad876026f8dce67e4b1adbf249aa0a6fc8914
Nikk <nikkigazejr@gmail.com>
2021-10-17 14:50:30 +0300

    initial

---
+++ Assets/Scripts/BirdFlyer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdFlyer : MonoBehaviour
+{
+    [SerializeField] private List<Transform> points;
+
+    private int _nextPointNumber;
+    private int _nextPointDirection;
+    private float _passDistance = 0.1f;
+    void Start()
+    {
+        _nextPointNumber = 1;
+        _nextPointDirection = 1;
+    }
+
+    void Update()
+    {
+        Transform nextPoint = points[_nextPointNumber];
+        transform.position = Vector3.MoveTowards(transform.position, nextPoint.position, 0.1f);
+
+        if (Vector3.Distance(transform.position, nextPoint.position) < _passDistance)
+        {
+            _nextPointNumber += _nextPointDirection;
+            if (_nextPointNumber == points.Count - 1 || _nextPointNumber == 0)
+            {
+                _nextPointDirection = -_nextPointDirection;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EstafeteLogic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class EstafeteLogic : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _runners;
+    [SerializeField] private Transform prize;
+
+    private Transform _currentRunner;
+    private int _nextPointNumber;
+    private float _passDistance = 2.0f;
+    void Start()
+    {
+        _nextPointNumber = 1;
+        _currentRunner = _runners[0];
+        _currentRunner.LookAt(_runners[_nextPointNumber]);
+    }
+
+    void Update()
+    {
+        Transform nextRunnerPosition = _runners[_nextPointNumber];
+        _currentRunner.position = Vector3.MoveTowards(_currentRunner.position, nextRunnerPosition.position, 0.1f);
+
+        if (Vector3.Distance(_currentRunner.position, nextRunnerPosition.position) < _passDistance)
+        {
+            _currentRunner = _runners[_nextPointNumber];
+            prize.SetParent(_currentRunner);
+            prize.transform.localPosition = Vector3.zero;
+            _nextPointNumber += 1;
+            
+            if (_nextPointNumber == _runners.Count )
+            {
+                _nextPointNumber = 0;
+            }
+            _currentRunner.LookAt(_runners[_nextPointNumber]);
+        }
+    }
+}
